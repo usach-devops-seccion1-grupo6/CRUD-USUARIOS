27ce3c40b8bb16927aee45a59278bda61cb04371
Gonzalo Muñoz <gmboisier@gmail.com>
2021-12-07 19:26:24 -0300

    Se agrega el metodo put

--- app/usuarios/api_v1_0/resources.py
+++ app/usuarios/api_v1_0/resources.py
@@ -10,3 +10,28 @@ usuarios_v1_0_bp = Blueprint('usuarios_v1_0_bp', __name__)
 usuario_schema = UsuarioSchema()
 api = Api(usuarios_v1_0_bp)
 
+class UsuarioResource(Resource):
+    def put(self, email):
+        """Permite modificar un usuario"""
+        temp = request.get_json()
+        try:
+            temp['email'] = email
+            data = usuario_schema.load(temp)
+
+            usuario = Usuario.simple_filter_one(email=email)
+            if usuario is None:
+                raise ObjectNotFound('El usuario no existe')
+
+        except ValidationError as err:
+            raise ObjectNotFound(err.messages)
+
+        usuario.nombre = data['nombre']
+        usuario.clave = data['clave']
+
+        usuario.update()
+        return ''
+
+api.add_resource(
+    UsuarioResource,
+    '/api/v1.0/usuarios/<string:email>',
+    endpoint='usuario_resource')
