67cadcb07ec39bcf70d516d2040cb2edd68d446f
Flamel Canto <flamel.canto@deepsource.cl>
2021-12-07 19:27:45 -0300

    añadida feature delete

--- app/usuarios/api_v1_0/resources.py
+++ app/usuarios/api_v1_0/resources.py
@@ -10,3 +10,20 @@ usuarios_v1_0_bp = Blueprint('usuarios_v1_0_bp', __name__)
 usuario_schema = UsuarioSchema()
 api = Api(usuarios_v1_0_bp)
 
+class UsuarioResource(Resource):
+    """Permite administrar un usuario"""
+
+
+    def delete(self, email):
+        """Permite eliminar un usuario"""
+        usuario = Usuario.simple_filter_one(email=email)
+        if usuario is None:
+            raise ObjectNotFound('El usuario no existe')
+
+        usuario.delete()
+        return '', 204
+
+api.add_resource(
+    UsuarioResource,
+    '/api/v1.0/usuarios/<string:email>',
+    endpoint='usuario_resource')
