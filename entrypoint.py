c384d7e6ac7ac816a41dff1e94938ccf85945d63
Eduardo Cabrera <lalo@eof.cl>
2021-12-13 22:32:59 -0300

    configuracion desde .env

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,9 @@
 """Archivo lanzador de aplicación"""
+import os
+from dotenv import load_dotenv
 from app import create_app
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 app = create_app()
